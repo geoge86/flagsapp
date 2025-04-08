001343f9fe0b0279da868b2ce6395dad543c7b9d
george <g_k_georgiadis@yahoo.gr>
2025-04-08 15:38:35 +0300

    add button

--- app/src/main/java/gr/ihu/flags/PhotoViewHolder.java
+++ app/src/main/java/gr/ihu/flags/PhotoViewHolder.java
@@ -1,6 +1,7 @@
 package gr.ihu.flags;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ public class PhotoViewHolder extends RecyclerView.ViewHolder {
 
     TextView flagcountry;
     TextView flagcontinent;
+
     public PhotoViewHolder(@NonNull View itemView) {
         super(itemView);
         flagcountry = itemView.findViewById(R.id.flagcountry);
